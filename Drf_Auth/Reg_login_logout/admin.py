ae523dfe212803e19b7d441fb77724b7c45111c3
Omar Faruk <omarfaruk18115@gmail.com>
2022-02-11 23:44:32 +0600

    version 2 update

--- Drf_Auth/Reg_login_logout/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from Reg_login_logout.models import UserProfile, Survey, Friend, UploadFile
-
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(Survey)
-admin.site.register(Friend)
-admin.site.register(UploadFile)
