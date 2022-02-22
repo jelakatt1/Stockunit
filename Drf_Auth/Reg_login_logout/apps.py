40ae6303d53a53b3fbf97b178bb3755871ca1a84
Omar Faruk <omarfaruk18115@gmail.com>
2022-02-22 23:52:21 +0600

    update payment

--- Drf_Auth/Reg_login_logout/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegLoginLogoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Reg_login_logout'
--- Drf_Auth/payment/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PaymentView
-
-urlpatterns = [
-    path('', PaymentView.as_view()),
-]
